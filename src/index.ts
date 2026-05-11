d55ea3944f140cb8420b8b392fa1c14bebbe0949
Qingyu Wang <40660121+colinaaa@users.noreply.github.com>
2026-05-11 17:28:30 +0800

    Type Rspack compiler in plugin

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import type { Compiler } from "@rspack/core";
 import { fileURLToPath } from "node:url";
 import { DEFAULT_EXCLUDE, DEFAULT_INCLUDE, type LoaderOptions, type Options } from "./options.js";
 
@@ -8,15 +9,13 @@ class TypiaRspackPlugin {
     this.#options = options;
   }
 
-  apply(compiler: any): void {
+  apply(compiler: Compiler): void {
     const options = this.#options;
     const loaderOptions: LoaderOptions = {
       tsconfig: options.tsconfig,
       typia: options.typia,
     };
 
-    compiler.options.module ??= {};
-    compiler.options.module.rules ??= [];
     compiler.options.module.rules.push({
       enforce: options.enforce ?? "pre",
       exclude: options.exclude ?? DEFAULT_EXCLUDE,
